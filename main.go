492bd2d2468684304021eb2eb7d08d50809f3b07
Danton Hastenreiter <dantonrafaeldr123@gmail.com>
2021-07-17 03:56:07 -0300

    Get list of brute data from my followers

--- main.go
+++ main.go
@@ -3,16 +3,19 @@ package main
 // OAuth1
 import (
 	"os"
+	"fmt"
+	"reflect"
+	"encoding/json"
 
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 )
 
 // Get my environment credentials
-const consumerSecret = os.Getenv("API_SECRET")
-const consumerSecret = os.Getenv("API_KEY")
-const accessToken = os.Getenv("ACCESS_TOKEN")
-const accessSecret = os.Getenv("ACCESS_SECRET")
+var consumerSecret = os.Getenv("API_SECRET")
+var consumerKey = os.Getenv("API_KEY")
+var accessToken = os.Getenv("ACCESS_TOKEN")
+var accessSecret = os.Getenv("ACCESS_SECRET")
 
 var config = oauth1.NewConfig(consumerKey, consumerSecret)
 var token = oauth1.NewToken(accessToken, accessSecret)
@@ -22,3 +25,12 @@ var httpClient = config.Client(oauth1.NoContext, token)
 
 // twitter client
 var client = twitter.NewClient(httpClient)
+
+
+
+func main()  {
+	followers, _, _ := client.Followers.List(&twitter.FollowerListParams{})
+	fmt.Println(reflect.TypeOf(followers))
+	users, _ := json.Marshal(followers)
+	fmt.Println(string(users))
+}
